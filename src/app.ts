30ba3efdf79f3b44ca7b19ee94333087fdf89d7a
Harsha Narayana <harsha.narayana@ge.com>
2018-05-28 21:36:03 +0530

    Heroku port fix

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import { Response, Request } from "express";
 
 import * as bodyParser from "body-parser";
 
@@ -8,29 +9,30 @@ import apiV1Routes from "./routes/apiV1";
 
 const app = express();
 
-const allowCrossDomain = function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+const allowCrossDomain = function(req: Request, res: Response, next) {
+  console.log(req.headers);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
 
-    // intercept OPTIONS method
-    if ("OPTIONS" == req.method) {
-        res.send(200);
-    }
-    else {
-        next();
-    }
+  // intercept OPTIONS method
+  if ("OPTIONS" == req.method) {
+    console.log("HERE");
+    res.send(200);
+  } else {
+    next();
+  }
 };
-
+app.options("*", cors());
 app.disable("etag");
 app.use(cors());
 app.use(allowCrossDomain);
 app.use(morgan("combined"));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Setup Application
-app.set("port", process.env.PORT || 3032);
+app.set("port", process.env.PORT || 5000);
 
 app.use("/api/v1", apiV1Routes);
 
